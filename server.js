24945815e11316e75b532925d0388e1fe0083fd5
maylopezga <maye.lopez16@gmail.com>
2018-11-14 22:09:50 -0500

    Changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var request = require('request');
 var moment = require('moment');
 const http = require('http');
 
-var m = "";
+var event = "";
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -14,15 +14,19 @@ app.get("/", (request, response) => {
 });
 
   // var myText = getNotification();
-
+  // setInterval(() => {
+  //   console.log('m: ', event);
+  //   event = getMessage();
+  //   console.log('m2: ', event);
+  // }, 8000)
+              
   setInterval(() => {
     console.log('This is');
-    const mytext = getNotification();
+    event = getMessage();
+    // const mytext = getNotification();
     // ll;;;;
     request.post('https://hooks.slack.com/services/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', {
-      json: {
-        text: m
-      }
+      json: event
     }, 
      
     (error, res, body) => {
@@ -148,45 +152,88 @@ app.post('/next-event', function (req, res) {
         });
   });
     
-// function getMessage() {
-//   let reply = {};
-//   let attachment = {},
-//   getNotifications()
-//         .then(events => ;
-//              if(events.length == 0){
-//                reply.text = 'No Meetups found in  :sleuth_or_spy: .\nMake sure the location you entered is correct and try again.:slightly_smiling_face:';
-//                return res.json(reply);
-//              }
-//              reply.text = 'Hey ,\nThis is the list of the envens.\nIf you want to go the event, click in the name of event and confirm the assistance in Meetup';
-//              events.forEach(event => {
-//                var status = (event.status != undefined) ? ('Status - '+event.status) : 'Status - visible only to Members';
-//                var date = new Date(event.time + event.utc_offset);
-//                date = moment(date).format('lll');
-//                var venue = (event.venue != undefined) ? event.venue.address_1 : 'Only visible to members';
-//                attachment.push({
-//                  title: 'Group - '+event.group.name,
-//                  text: '<'+event.link+'| Event - '+event.name+'>',
-//                  author_name: status,
-//                  title_link: 'https://www.meetup.com/'+event.group.urlname,
-//                  color: "#764FA5",
-//                  fields: [
-//                    { "title": "Date", "value": date, "short": true },
-//                    { "title": "Venue", "value": venue, "short": true },
-//                    { "title": "RSVP Count", "value": event.yes_rsvp_count, "short": true }
-//                  ]
-//                });
-//              });
-//              reply.attachments = attachment;
-//              return res.json(reply);
-//                 myText = reply;
-//           })
-//            .catch(e => {
-//              console.log("Occured an error in getEvent. " + e);
-//                 myText = 'Error '+ e;
-//              return res.json({text: 'Ops Occurred an error. Please try again'});
-//           });
+async function getMessage() {
+  let reply = {};
+  let attachment = {};
+  let myActualTime = moment();
+  // actualTime = new Date(actualTime+ event.utc_offset);
+  console.log(myActualTime);
+  event = await getEvents('node_co');
+  console.log(typeof event)
+  if (event.length === 0){
+    event = 'There are not events';
+  } else {
+    reply.text = 'The next event is...'
+    event.forEach(event => {
+      const actualTime = new Date (myActualTime + event.utc_offset);
+      console.log(actualTime);
+      const dateEvent = new Date(event.time + event.utc_offset);
+      console.log(dateEvent);
+      if (dateEvent > actualTime) {
+        var status = (event.status != undefined) ? ('Status - '+event.status) : 'Status - visible only to Members';
+        var venue = (event.venue != undefined) ? event.venue.address_1 : 'Only visible to members';
+        console.log('Es mayor');
+        console.log('Es mayor');
+        attachment = {
+         title: 'Group - '+event.group.name,
+         text: '<'+event.link+'| Event - '+event.name+'>',
+         author_name: status,
+         title_link: 'https://www.meetup.com/'+event.group.urlname,
+         color: "#764FA5",
+         fields: [
+           { "title": "Date", "value": dateEvent, "short": true },
+           { "title": "Venue", "value": venue, "short": true },
+           { "title": "RSVP Count", "value": event.yes_rsvp_count, "short": true }
+          ]
+        };
+        reply.attachments = attachment;
+        console.log(reply);
+        return reply;
+      }
+    })
+    return 'There are not events';
+  }
+  // console.log('....................................................................................', event );
+  
  
-// }
+       // .then(events => {
+        //      if(events.length == 0){
+        //        reply.text = 'No Meetups found in  :sleuth_or_spy: .\nMake sure the location you entered is correct and try again.:slightly_smiling_face:';
+        //        // return res.json(reply);
+        //        return reply;
+        //      }
+        //      reply.text = 'Hey ,\nThis is the list of the envens.\nIf you want to go the event, click in the name of event and confirm the assistance in Meetup';
+        //      events.forEach(event => {
+        //        var status = (event.status != undefined) ? ('Status - '+event.status) : 'Status - visible only to Members';
+        //        var date = new Date(event.time + event.utc_offset);
+        //        date = moment(date).format('lll');
+        //        var venue = (event.venue != undefined) ? event.venue.address_1 : 'Only visible to members';
+        //        attachment.push({
+        //          title: 'Group - '+event.group.name,
+        //          text: '<'+event.link+'| Event - '+event.name+'>',
+        //          author_name: status,
+        //          title_link: 'https://www.meetup.com/'+event.group.urlname,
+        //          color: "#764FA5",
+        //          fields: [
+        //            { "title": "Date", "value": date, "short": true },
+        //            { "title": "Venue", "value": venue, "short": true },
+        //            { "title": "RSVP Count", "value": event.yes_rsvp_count, "short": true }
+        //          ]
+        //        });
+        //      });
+        //      reply.attachments = attachment;
+        //       // myText = reply;
+        //      // return res.json(reply);
+        //      return reply;
+        //   })
+        //    .catch(e => {
+        //      console.log("Occured an error in getEvent. " + e);
+        //      // myText = 'Error '+ e;
+        //      // return res.json({text: 'Ops Occurred an error. Please try again'});
+        //      return 'Error '+ e;
+        //   });
+ 
+}
 
 /*
 * function to get events of meetup using meetup API
@@ -217,6 +264,8 @@ function getEvents(nameMeetup) {
     });
 
   });
+  
+  // return m;
 }
 
 /*
@@ -236,12 +285,13 @@ function getNotifications() {
 
       console.log(options, ' options');
       request(options, function (error, response, body) {
+        console.log('estestes', error);
         if (error) {
           console.log("error occured in getEvents as "+error);
           reject('Erorr');
         } else {
           // body = JSON.parse(body);
-          console.log(body.length);
+          console.log('.;.;.; ', body.length, ' /// ', body);
           m = body;
           resolve(body);
         }
