b66239906aeb5b7bbf99dade777245e1f492006e
maylopezga <maye.lopez16@gmail.com>
2018-11-15 22:16:14 -0500

    Change

--- server.js
+++ server.js
@@ -3,16 +3,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 var moment = require('moment');
-var querystring = require('querystring');
 const http = require('http');
 
 var event = "";
-var id = '';
-var token = '';
-
-
-var formData = querystring.stringify(form);
-var formData2 = querystring.stringify(ff);
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,32 +13,27 @@ app.get("/", (request, response) => {
   response.sendStatus(200);
 });
 
-/* Notification of the next event */
-// setInterval (async ()  => {
-//   const m = await getMessage();
-//   request.post('https://hooks.slack.com/services/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', {
-//     json:{ text: m }
-//     kkk
-//   }, 
+setInterval (async ()  => {
+  const myText = await getMessage();
+  request.post('https://hooks.slack.com/services/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', {
+    json:{ text: myText }
+  }, 
   
-//   (error, res, body) => {
-//     if (error) {
-//       console.error(error)
-//       return
-//     }
-//     console.log(`statusCode: ${res.statusCode}`)
-//     console.log(body)
-//   })
-// }, 30000);
+  (error, res, body) => {
+    if (error) {
+      console.error(error)
+      return
+    }
+    console.log(`statusCode: ${res.statusCode}`)
+    console.log(body)
+  })
+}, 30000);
 
 /*
 * /hello_mybot calls the meetupbot and response is a greeting along
 */
 app.post('/meetupbot', function (req, res) {
   var userName = req.body.user_name;
-  id = req.body.user_id;
-  token = req.body.token;
-  console.log(req.body);
   var attachment=[];
   var reply = {};
   var nameMeetup = 'node_co';
@@ -60,8 +48,6 @@ app.post('/meetupbot', function (req, res) {
       }
     ]
   };
-  
-  getUserInfo();
   res.json(reply);
 });
 
@@ -81,7 +67,6 @@ app.post('/next-event', function (req, res) {
             return res.json(reply);
           }
           reply.text = 'Hey '+userName+',\nThis is the list of the envens.\nIf you want to go the event, click in the name of event and confirm the assistance in Meetup';
-          console.log('m.m');
           events.forEach(event => {
             var status = (event.status != undefined) ? ('Status - '+event.status) : 'Status - visible only to Members';
             var date = new Date(event.time + event.utc_offset);
@@ -188,71 +173,6 @@ function getEvents(nameMeetup) {
   });
 }
 
-function getUserInfo() {
-  var key = process.env.SECRET;
-  
-  return new Promise((resolve, reject) => {
-    var options = {
-      method: 'POST',
-      url: 'https://slack.com/api/oauth.token',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: formData2,
-      // params: {
-      //   token: 'xoxp-27923301078-27973812067-481398567061-04c46bd33fa0c9b11b96b75395fe43f8',
-      //   user: id,
-      // }
-    };
-
-    console.log(options, ' options');
-    request(options, function (error, response, body) {
-      if (error) {
-        console.log("error occured in getEvents as "+error);
-        reject('Erorr');
-      } else {
-        console.log('dddd888998', body)
-        console.log(response.statusCode)
-        body = JSON.parse(body);
-        console.log(body.length);
-        resolve(body);
-      }
-    });
-    
-
-  });
-}
-/*
-// * function to get the notifications
-function getNotifications() {
-    var key = process.env.SECRET;
-    console.log('Notifi');
-    const value =  new Promise((resolve, reject) => {
-      var options = {
-        method: 'GET',
-        url: 'https://api.meetup.com/notifications',
-        params: {
-          key: key
-        }
-      };
-
-      console.log(options, ' options');
-      request(options, function (error, response, body) {
-        console.log('estestes', error);
-        if (error) {
-          console.log("error occured in getEvents as "+error);
-          reject('Erorr');
-        } else {
-          // body = JSON.parse(body);
-          console.log('.;.;.; ', body.length, ' /// ', body);
-          // m = body;
-          resolve(body);
-        }
-      });
-
-    });
-} */
-
 /*
 * function to get the confirmation of event of meetup using meetup API
 */
@@ -284,6 +204,7 @@ function getConfirmation(nameMeetup, idEvent) {
 
   });
 }
+
 /*
 *function to get meetups using meetup API
 */
